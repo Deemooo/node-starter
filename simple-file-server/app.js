09a3493812273b6065e837f760ed9360c4da9fed
Deemooo <yibiluo@foxmail.com>
2019-01-02 23:45:47 +0800

    简单爬虫

--- simple-file-server/app.js
+++ simple-file-server/app.js
@@ -1,46 +1,24 @@
-var eventproxy = require('eventproxy');
-var superagent = require('superagent');
-var cheerio = require('cheerio');
-var url = require('url');
+var async = require('async');
 
-var cnodeUrl = 'https://cnodejs.org/';
+var count = 0;
+var fetchUrl = function (url, callback) {
+    var delay = parentInt((Math.random() * 10000000) % 2000, 10);
+    count++;
+    console.log('现在的并发数是', concurrencyCount, '，正在抓取的是', url, '，耗时' + delay + '毫秒');
+    setTimeout(function () {
+       count--;
+       callback(null, url + 'html content');
+    }, delay);
+};
 
-superagent.get(cnodeUrl)
-    .end(function (err, res) {
-        if (err) {
-            return console.error(err);
-        }
-        var topicUrls = [];
-        var $ = cheerio.load(res.text);
-        $('#topic_list .topic_title').each(function (idx, element) {
-            var $element = $(element);
-            var href = url.resolve(cnodeUrl, $element.attr('href'));
-            topicUrls.push(href);
-        });
+var urls = [];
+for (var i = 0; i < 30; i++) {
+    urls.push('http://datasource_' + i);
+}
 
-        var ep = new eventproxy();
-
-        ep.after('topic_html', topicUrls.length, function (topics) {
-            topics = topics.map(function (topicPair) {
-                var topicUrl = topicPair[0];
-                var topicHtml = topicPair[1];
-                var $ = cheerio.load(topicHtml);
-                return ({
-                    title: $('.topic_full_title').text().trim(),
-                    href: topicUrl,
-                    comment1: $('.reply_content').eq(0).text().trim(),
-                });
-            });
-
-            console.log('final:');
-            console.log(topics);
-        });
-
-        topicUrls.forEach(function (topicUrl) {
-            superagent.get(topicUrl)
-                .end(function (err, res) {
-                    console.log('fetch ' + topicUrl + ' successful');
-                    ep.emit('topic_html', [topicUrl, res.text]);
-                });
-        });
-    });
+async.mapLimit(urls, 5, function (url, callback) {
+    fetchUrl(url, callback);
+}, function (err, result) {
+    console.log('final:');
+    console.log(result);
+});
