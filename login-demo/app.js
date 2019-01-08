02eb0f66f1bbcea9d41b2b1e4e1b53d03bad9992
Deemooo <yibiluo@foxmail.com>
2019-01-09 00:42:11 +0800

    koa-router

--- login-demo/app.js
+++ login-demo/app.js
@@ -1,12 +1,22 @@
+// 按照官方示例
 const Koa = require('koa');
 const app = new Koa();
+const router = require('koa-router')();
 
-app.use(async (ctx, next) => {
-    await next();
-    ctx.response.type = 'text/html';
-    ctx.response.body = '<h1>Hello World</h1>>';
+router.get('/', async (ctx, next) => {
+    ctx.response.body = '<h1>index page</h1>';
 });
 
+router.get('/home', async (ctx, next) => {
+    ctx.response.body = '<h1>HOME page</h1>';
+});
+
+router.get('/404', async (ctx, next) => {
+    ctx.response.body = '<h1>Not Found</h1>';
+});
+
+app.use(router.routes());
+
 app.listen(3000, () => {
-    console.log('server is running at http://localhost:3000');
+    console.log('server is running at http://localhost:3000')
 });
